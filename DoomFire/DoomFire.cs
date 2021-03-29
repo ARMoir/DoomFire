80d5289b4445226b0b18a3de22cd0d3fedae9bf2
Alex Moir <alexrmoir@gmail.com>
2021-03-29 13:58:30 -0400

    2.1.3.29

--- DoomFire/DoomFire.cs
+++ DoomFire/DoomFire.cs
@@ -10,23 +10,14 @@ namespace DoomFire
 {
     public partial class DoomFire : Form
     {
-        public static class Globals
-        {
-            public static int CJS_TICKER_FPS = 27;
-
-            public static int FIRE_WIDTH = 256;
-
-            public static int FIRE_HEIGHT = 128;
-
-            public static Random random = new Random();
-
-            public static Bitmap bitmap;
-
-            public static Color[] firePal = new Color[37];
-
-            public static int[] firePixels = new int[Globals.FIRE_WIDTH * Globals.FIRE_HEIGHT];
-
-            public static int[] fireRGB = {
+        public static Random RANDOM { get; set; } = new Random();
+        public static Bitmap BITMAP { get; set; }
+        public static int CJS_TICKER_FPS { get; set; } = 27;
+        public static int FIRE_WIDTH { get; set; } = 256;
+        public static int FIRE_HEIGHT { get; set; } = 128;
+        public static Color[] firePal { get; set; } = new Color[37];
+        public static int[] firePixels { get; set; } = new int[FIRE_WIDTH * FIRE_HEIGHT];
+        public static int[] fireRGB { get; set; } = {
                 0x07, 0x07, 0x07, 0x1F, 0x07, 0x07, 0x2F, 0x0F, 0x07, 0x47, 0x0F, 0x07, 0x57, 0x17, 0x07, 0x67,
                 0x1F, 0x07, 0x77, 0x1F, 0x07, 0x8F, 0x27, 0x07, 0x9F, 0x2F, 0x07, 0xAF, 0x3F, 0x07, 0xBF, 0x47,
                 0x07, 0xC7, 0x47, 0x07, 0xDF, 0x4F, 0x07, 0xDF, 0x57, 0x07, 0xDF, 0x57, 0x07, 0xD7, 0x5F, 0x07,
@@ -34,46 +25,34 @@ namespace DoomFire
                 0x87, 0x17, 0xC7, 0x87, 0x17, 0xC7, 0x8F, 0x17, 0xC7, 0x97, 0x1F, 0xBF, 0x9F, 0x1F, 0xBF, 0x9F,
                 0x1F, 0xBF, 0xA7, 0x27, 0xBF, 0xA7, 0x27, 0xBF, 0xAF, 0x2F, 0xB7, 0xAF, 0x2F, 0xB7, 0xB7, 0x2F,
                 0xB7, 0xB7, 0x37, 0xCF, 0xCF, 0x6F, 0xDF, 0xDF, 0x9F, 0xEF, 0xEF, 0xC7, 0xFF, 0xFF, 0xFF };
-        }
+
 
         public DoomFire()
         {
             InitializeComponent();
-
-            Ticker.Interval = Globals.CJS_TICKER_FPS;
-
-            Globals.bitmap = new Bitmap(Convert.ToInt32(Globals.FIRE_WIDTH), Convert.ToInt32(Globals.FIRE_HEIGHT), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Ticker.Interval = CJS_TICKER_FPS;
+            BITMAP = new Bitmap(Convert.ToInt32(FIRE_WIDTH), Convert.ToInt32(FIRE_HEIGHT), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
             for (var i = 0; i < 37; i++)
             {
-
-                Globals.firePal[i] = Color.FromArgb((Globals.fireRGB[i * 3 + 0]), (Globals.fireRGB[i * 3 + 1]), (Globals.fireRGB[i * 3 + 2]));
-
+                firePal[i] = Color.FromArgb((fireRGB[i * 3 + 0]), (fireRGB[i * 3 + 1]), (fireRGB[i * 3 + 2]));
             }
 
-            for (var i = 0; i < Globals.FIRE_WIDTH * Globals.FIRE_HEIGHT; i++)
+            for (var i = 0; i < FIRE_WIDTH * FIRE_HEIGHT; i++)
             {
-
-                Globals.firePixels[i] = 0;
-
+                firePixels[i] = 0;
             }
 
-            for (var i = 0; i < Globals.FIRE_WIDTH; i++)
+            for (var i = 0; i < FIRE_WIDTH; i++)
             {
-
-                Globals.firePixels[(Globals.FIRE_HEIGHT - 1) * Globals.FIRE_WIDTH + i] = 36;
-
+                firePixels[(FIRE_HEIGHT - 1) * FIRE_WIDTH + i] = 36;
             }
-
         }
 
         public int DrawPixel(int x, int y, int pixel)
         {
-
-            Globals.bitmap.SetPixel(x, y, Globals.firePal[pixel]);
-
+            BITMAP.SetPixel(x, y, firePal[pixel]);
             return pixel;
-
         }
 
         public int SpreadFire(int pixel, int curSrc, int counter, int srcOffset, int rand, int width)
@@ -81,23 +60,15 @@ namespace DoomFire
 
             if (pixel != 0)
             {
-
-                int randIdx = Globals.random.Next(0, 255);
-
+                int randIdx = RANDOM.Next(0, 255);
                 int tmpSrc;
-
                 rand = ((rand + 2) & 255);
-
                 tmpSrc = (curSrc + (((counter - (randIdx & 3)) + 1) & (width - 1)));
-
-                Globals.firePixels[tmpSrc - Globals.FIRE_WIDTH] = pixel - (randIdx & 1);
-
+                firePixels[tmpSrc - FIRE_WIDTH] = pixel - (randIdx & 1);
             }
             else
             {
-
-                Globals.firePixels[srcOffset - Globals.FIRE_WIDTH] = 0;
-
+                firePixels[srcOffset - FIRE_WIDTH] = 0;
             }
 
             return rand;
@@ -107,79 +78,53 @@ namespace DoomFire
         {
 
             int counter = 0;
-
-            int rand = Globals.random.Next(0, 255);
-
-            int curSrc = Globals.FIRE_WIDTH;
+            int rand = RANDOM.Next(0, 255);
+            int curSrc = FIRE_WIDTH;
 
             do
             {
-
                 int srcOffset = (curSrc + counter);
-
-                int pixel = Globals.firePixels[srcOffset];
-
+                int pixel = firePixels[srcOffset];
                 int step = 2;
-
-                rand = SpreadFire(pixel, curSrc, counter, srcOffset, rand, Globals.FIRE_WIDTH);
-
-                curSrc += Globals.FIRE_WIDTH;
-
-                srcOffset += Globals.FIRE_WIDTH;
+                rand = SpreadFire(pixel, curSrc, counter, srcOffset, rand, FIRE_WIDTH);
+                curSrc += FIRE_WIDTH;
+                srcOffset += FIRE_WIDTH;
 
                 do
                 {
-
-                    pixel = Globals.firePixels[srcOffset];
-
+                    pixel = firePixels[srcOffset];
                     step += 2;
+                    rand = SpreadFire(pixel, curSrc, counter, srcOffset, rand, FIRE_WIDTH);
+                    pixel = firePixels[srcOffset + FIRE_WIDTH];
+                    curSrc += FIRE_WIDTH;
+                    srcOffset += FIRE_WIDTH;
+                    rand = SpreadFire(pixel, curSrc, counter, srcOffset, rand, FIRE_WIDTH);
+                    curSrc += FIRE_WIDTH;
+                    srcOffset += FIRE_WIDTH;
 
-                    rand = SpreadFire(pixel, curSrc, counter, srcOffset, rand, Globals.FIRE_WIDTH);
-
-                    pixel = Globals.firePixels[srcOffset + Globals.FIRE_WIDTH];
-
-                    curSrc += Globals.FIRE_WIDTH;
-
-                    srcOffset += Globals.FIRE_WIDTH;
-
-                    rand = SpreadFire(pixel, curSrc, counter, srcOffset, rand, Globals.FIRE_WIDTH);
-
-                    curSrc += Globals.FIRE_WIDTH;
-
-                    srcOffset += Globals.FIRE_WIDTH;
-
-                } while (step < Globals.FIRE_HEIGHT);
+                } while (step < FIRE_HEIGHT);
 
                 counter++;
+                curSrc -= ((FIRE_WIDTH * FIRE_HEIGHT) - FIRE_WIDTH);
 
-                curSrc -= ((Globals.FIRE_WIDTH * Globals.FIRE_HEIGHT) - Globals.FIRE_WIDTH);
-
-            } while (counter < Globals.FIRE_WIDTH);
+            } while (counter < FIRE_WIDTH);
         }
 
         private void Ticker_Tick(object sender, EventArgs e)
         {
-
             DoFire();
 
-            for (var h = 0; h < Globals.FIRE_HEIGHT; h++)
+            for (var h = 0; h < FIRE_HEIGHT; h++)
             {
-
-                for (var w = 0; w < Globals.FIRE_WIDTH; w++)
+                for (var w = 0; w < FIRE_WIDTH; w++)
                 {
-
-                    var p = Globals.firePixels[h * Globals.FIRE_WIDTH + w];
-
+                    var p = firePixels[h * FIRE_WIDTH + w];
                     DrawPixel(w, h, p);
-
                 }
-
             }
 
-            Stage.SizeMode = PictureBoxSizeMode.StretchImage;
-
-            Stage.Image = Globals.bitmap;
-
+            STAGE.SizeMode = PictureBoxSizeMode.StretchImage;
+            STAGE.Image = BITMAP;
         }
     }
 }
